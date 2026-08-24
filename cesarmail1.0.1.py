45b89672a6eb13a36a89fe57f945f05ed22dce4a
CesarMatias1245 <cesarmatiascanales@gmail.com>
2026-08-24 14:24:13 -0300

    Arreglado el bug de la linea 44

---
+++ cesarmail1.0.1.py
@@ -0,0 +1,44 @@
+import os
+import time
+
+archivo_config = "config.txt"
+
+if not os.path.exists(archivo_config):
+    input("nombre: ")
+    input("correo: ")
+    input("contraseña: ")
+    # :)
+
+    with open(archivo_config, "w") as archivo:
+        archivo.write("Preguntas iniciales realizadas")
+
+while True:
+    print("\n--- César Mail™ ---")
+    print("1. Bandeja de entrada")
+    print("2. Enviados")
+    print("3. Enviar")
+    print("4. Salir")
+
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        print("Hay mail=yo que se.")
+        time.sleep(1)
+
+    elif opcion == "2":
+        print("enviaste algo=yo que se.")
+        time.sleep(1)
+
+    elif opcion == "3":
+        input("A quien: ")
+        input("Asunto: ")
+        input("El resto del correo: ")
+        print("Se envio=yo que se")
+        time.sleep(1)
+
+    elif opcion == "4":
+        break
+
+    else:
+        print("Escribiste algo bien=yo que se")
+        time.sleep(1)
